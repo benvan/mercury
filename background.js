2d38d83d806e4e6b49b8cd69dfeea09d98651abd
Ben van Enckevort <ben.van.enckevort@zeebox.com>
2014-01-18 20:50:11 +0000

    explained background page

--- background.js
+++ background.js
@@ -1,1 +1,2 @@
+// having a background page noticeably reduces the delay between invoking the popup and it appearing. It's not ideal, but I haven't yet found an alternative.
 console.log('running');
