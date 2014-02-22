c070461b74d467dc87be25be0919a8197d65ca3f
Ben van Enckevort <ben.van.enckevort@zeebox.com>
2014-02-22 15:07:13 +0000

    added ctrl-p/n navigation. fixes #1 . also fixed indentation. messy diff, oops.

--- mercury.js
+++ mercury.js
@@ -1,4 +1,5 @@
 
+var noop = function(){};
 var pick = function(key){ return function(obj){ return obj[key]; }; };
 var indexOf = function(match){ return function(obj){ return obj.indexOf(match) }};
 
@@ -13,30 +14,30 @@ var tpl = document.getElementById('row').innerHTML;
 
 
 var row = function(tab){
-	var replace = function(context, key){ return context.replace('{{'+key+'}}',tab[key]); }
-	return ['title','url','favIconUrl'].reduce(replace, tpl);
+    var replace = function(context, key){ return context.replace('{{'+key+'}}',tab[key]); }
+    return ['title','url','favIconUrl'].reduce(replace, tpl);
 };
 
 var getInput = function(){ return search.value.toLowerCase() };
 
 var renderTabs = function(tabs){
-	container.innerHTML = tabs.map( row ).join('');
+    container.innerHTML = tabs.map( row ).join('');
 };
 
 var selectResult = function(index){
-	[].slice.apply(container.children).forEach(function(child){
-		child.removeAttribute('class');
-	});
-	container.children[index].setAttribute('class', 'active');
-	container.children[index].scrollIntoView(false);
+    [].slice.apply(container.children).forEach(function(child){
+        child.removeAttribute('class');
+    });
+    container.children[index].setAttribute('class', 'active');
+    container.children[index].scrollIntoView(false);
 };
 
 var activate = function(tab){
-	chrome.tabs.update(tab.id, {active:true}, function(){
-		if (tab.windowId !== chrome.windows.WINDOW_ID_CURRENT){
-			chrome.windows.update(tab.windowId, {focused:true});
-		}
-	});
+    chrome.tabs.update(tab.id, {active:true}, function(){
+        if (tab.windowId !== chrome.windows.WINDOW_ID_CURRENT){
+            chrome.windows.update(tab.windowId, {focused:true});
+        }
+    });
 };
 
 var clickedIndex = function(ev){
@@ -48,70 +49,77 @@ var clickedIndex = function(ev){
 
 
 var score = function(terms){ return function(tab){
-	var titleTerms = tab.title.toLowerCase().split(tokenizer);
-	var urlTerms = tab.url.toLowerCase().split(tokenizer);
-	var allTerms = [].concat(titleTerms,urlTerms);
+    var titleTerms = tab.title.toLowerCase().split(tokenizer);
+    var urlTerms = tab.url.toLowerCase().split(tokenizer);
+    var allTerms = [].concat(titleTerms,urlTerms);
 
-	var matches = terms.map(function(term){
-		return allTerms.map( indexOf(term) ).indexOf(0);
-	});
-	var hasMisMatch = matches.indexOf(-1) >= 0 || !matches.length;
-	return {
-		score: hasMisMatch ? -1 : matches.reduce(function(a,b){ return a+b },0),
-		tab: tab
-	}
-}}
+    var matches = terms.map(function(term){
+        return allTerms.map( indexOf(term) ).indexOf(0);
+    });
+    var hasMisMatch = matches.indexOf(-1) >= 0 || !matches.length;
+    return {
+        score: hasMisMatch ? -1 : matches.reduce(function(a,b){ return a+b },0),
+        tab: tab
+    }
+}};
 
 var getSortedResults = function(tabInfos){
-	var inputTokens = getInput().split(tokenizer).filter(function(term){
-		return !term.match(tokenizer);
-	});
+    var inputTokens = getInput().split(tokenizer).filter(function(term){
+        return !term.match(tokenizer);
+    });
 
-	return getInput() == ""
-	? tabInfos
-	: tabInfos
-		.map( score(inputTokens) )
-		.filter( function(st){ return st.score != -1 } )
-		.sort( function(a,b){ return a.score - b.score } )
-		.map( pick('tab') );
+    return getInput() == ""
+    ? tabInfos
+    : tabInfos
+        .map( score(inputTokens) )
+        .filter( function(st){ return st.score != -1 } )
+        .sort( function(a,b){ return a.score - b.score } )
+        .map( pick('tab') );
 };
 
 chrome.tabs.query({}, function(tabInfos){
-	var filterAndDraw = function(){
-		results = getSortedResults(tabInfos);
-		renderTabs(results);
-		selectResult(0);
-	};
+    var filterAndDraw = function(){
+        results = getSortedResults(tabInfos);
+        renderTabs(results);
+        selectResult(0);
+    };
 
-	var index = 0;
-	var results = [];
+    var index = 0;
+    var results = [];
 
 
-  container.addEventListener('click', function(e){
-    index = clickedIndex(e);
-    activate(results[index]);
-  });
-	search.addEventListener('input', filterAndDraw );
-	search.addEventListener('keydown', function(ev){
-		if ([38,40].indexOf(ev.keyCode) >= 0 && results.length){
-			index += ({ 40: 1, 38: -1 }[ev.keyCode] || 0) + results.length;
-			index %= results.length;
-			selectResult(index);
-			ev.preventDefault();
-			return false;
-		} else if(ev.keyCode == 13){
-			activate(results[index]);
-		}
-	});
+    container.addEventListener('click', function(e){
+        index = clickedIndex(e);
+        activate(results[index]);
+    });
+    search.addEventListener('input', filterAndDraw );
+    search.addEventListener('keydown', function(ev){
+        var dir = function(x){ return function(){
+            index += x + results.length; index %= results.length;
+            selectResult(index);
+            ev.preventDefault();
+            return false;
+        }};
+        var ctrl = function(fn){ return ev.ctrlKey ? fn : noop };
 
-	filterAndDraw();
-	tablist.removeAttribute('class'); // remove heightHack
+        var cmds = {
+            38: dir(-1),
+            40: dir(1),
+            78: ctrl(dir(1)),
+            80: ctrl(dir(-1)),
+            13: function(){ activate(results[index]); }
+        };
 
+        return (cmds[ev.keyCode] || noop)();
+    });
+
+    filterAndDraw();
+    tablist.removeAttribute('class'); // remove heightHack
 
 });
 
 document.addEventListener('keydown', function(){
-	search.focus();
+    search.focus();
 });
 
 
